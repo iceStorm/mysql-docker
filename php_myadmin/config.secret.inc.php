dc8967fe75fc7a405d1acab81654e7885fe0749d
iceStorm <hauduedaiduong@gmail.com>
2022-03-27 23:24:29 +0700

    first commit

---
+++ php_myadmin/config.secret.inc.php
@@ -0,0 +1,2 @@
+<?php
+$cfg['blowfish_secret'] = 'f+gt9$-+mtmofDfp{R,0~Ry]F>HsPq46';
---
+++ php_myadmin/config.user.inc.php
@@ -0,0 +1,5 @@
+// $i++;
+// $cfg['Servers'][$i]['host'] = 'us-cdbr-east-05.cleardb.net/heroku_e228746099cef8c'; //provide hostname and port if other than default
+// $cfg['Servers'][$i]['user'] = 'b4c869353e3162';   //user name for your remote server
+// $cfg['Servers'][$i]['password'] = '01551ecd';  //password
+// $cfg['Servers'][$i]['auth_type'] = 'config';       // keep it as config
